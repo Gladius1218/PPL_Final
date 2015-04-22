f660bba72633c5a38aab477b238f76d4295243f7
Gladius <g2007531218@hotmail.com>
2015-04-23 04:14:07 +0800

    Pure_JS_version
    upload pure JS version

---
+++ Pure_JS/js/flyer.js
@@ -0,0 +1,201 @@
+//player class
+var Flyer = function(){
+	//player dom
+	this.dom = null;
+	//alive or not
+	this.isLive = true;
+	//is moving
+	this.isMove = false;
+	//move direction
+	this.moveId = null;
+	//firing?
+	this.isSend = false;
+	//total bullet in the screen
+	this.nowBullet = 0;
+	this.Lives = 3;
+	this.init();
+}
+Flyer.prototype = {
+	//game background dom
+	gamePanel : null,
+	//width
+	gameWidth : 0,
+	//height
+	gameHeight : 0,
+	//flight speed
+	movepx : 10,
+	//move frequency
+	movesp : 30,
+	//vullet level
+	bulletLevel : 1,
+	//max bullet number
+	maxBullet : 12,
+	//方向键值对应
+	keyCodeAndDirection : {
+		37 : "left",
+		38 : "up",
+		39 : "right",
+		40 : "down"
+	},
+	//init
+	init : function(){
+		this.dom = document.createElement('div');
+		this.dom.className = 'flyer';
+	},
+	//set position
+	setPosition : function(gamePanel,width,height){
+		//put player in the panel
+		this.gamePanel = gamePanel;
+		this.gamePanel.appendChild(this.dom);
+		//original position
+		this.dom.style.left = (width - this.dom.clientWidth) / 2 + 'px';
+		this.dom.style.top = height - this.dom.clientHeight + 'px';
+		//backgroung height & width
+		this.gameWidth = width;
+		this.gameHeight = height;
+	},
+	//key pressed function
+	keydown : function(e) {
+		
+		var keyCode = e.keyCode;
+		//press space to fire
+		if(keyCode == 32){
+			//if not firing
+			if(!this.isSend){
+				//fire
+				this.onSendBullet();
+				this.isSend = true;
+			}
+		}
+		//is moving
+		else if(!this.isMove)this.move(keyCode);
+	},
+	//key release event
+	keyup : function(e){
+		//key released
+		if(this.keyCodeAndDirection[e.keyCode]){
+			//stop moving
+			this.stopMove();
+		}
+		//space release
+		else if(e.keyCode == 32){
+			//set not firing
+			this.isSend = false;
+		}
+	},
+	//move
+	move : function(keyCode){
+		//set moving
+		this.isMove = true;
+		var _this = this;
+		//check direction
+		switch(this.keyCodeAndDirection[keyCode]){
+			case "left":{
+				
+				this.moveId = setInterval(function(){_this.moveLeftUp("left");},_this.movesp);
+				break;
+			}
+			case "up":{
+				
+				this.moveId = setInterval(function(){_this.moveLeftUp("up");},_this.movesp);
+				break;
+			}
+			case "right":{
+				
+				this.moveId = setInterval(function(){_this.moveRightDown("right")},_this.movesp);
+				break;
+			}
+			case "down":{
+				
+				this.moveId = setInterval(function(){_this.moveRightDown("down");},_this.movesp);
+				break;
+			}
+			default:break;
+		}
+		
+	},
+	//move left or up
+	moveLeftUp : function(direction){
+		
+		var leftOrUp = this.dom[direction=="left"?"offsetLeft":"offsetTop"];
+		leftOrUp = leftOrUp - this.movepx >= 0 ? leftOrUp - this.movepx:0;
+		this.dom.style[direction=="left"?"left":"top"] = leftOrUp + 'px';
+		
+		if(leftOrUp == 0){this.stopMove();}
+	},
+	//move right or down
+	moveRightDown : function(direction){
+		
+		var leftOrUp = this.dom[direction=="right"?"offsetLeft":"offsetTop"];
+		var maxDistance = direction=="right"?this.gameWidth-this.dom.clientWidth:this.gameHeight - this.dom.clientHeight;
+		leftOrUp = leftOrUp + this.movepx <= maxDistance ? leftOrUp + this.movepx:maxDistance;
+		this.dom.style[direction=="right"?"left":"top"] = leftOrUp + 'px';
+		
+		if(leftOrUp == maxDistance){this.stopMove();}
+	},
+	//stop moving
+	stopMove : function(){
+		this.isMove = false;
+		clearInterval(this.moveId);
+	},
+	//fire
+	sendBullet :function(enemyList){
+		//see bullet remain
+		if(this.bulletLevel + this.nowBullet > this.maxBullet){return;}
+		
+		var _this = this;
+		//fire according to bullet level
+		for (var i = 1,l=this.bulletLevel; i <= l; i++) {
+			//new bullet obj
+			var bullet = new Bullet();
+			//add dom to game panel
+			this.gamePanel.appendChild(bullet.dom);
+			//set original location
+			bullet.setPosition({
+				left: this.dom.offsetLeft,
+				top: this.dom.offsetTop,
+				width: this.dom.clientWidth,
+				position : i,
+				level : l
+			});
+			//check hit
+			bullet.checkBeat = function(){
+				//search enemy list
+				for (var i = 0, l = enemyList.length; i < l; i++) {
+					//dead enemy
+					if(!enemyList[i].isLive)continue;
+					//get enemy position and radiation, also bullet position and radiation
+					var e_left = enemyList[i].dom.offsetLeft, e_top = enemyList[i].dom.offsetTop, e_radius = enemyList[0].dom.clientWidth / 2, b_left = this.dom.offsetLeft, b_top = this.dom.offsetTop, b_radius = bullet.dom.clientWidth / 2;
+					//see contact or not
+					if (Math.sqrt(Math.pow(e_left - b_left, 2) + Math.pow(e_top - b_top, 2)) <= e_radius + b_radius) {
+						//enemy down
+						enemyList[i].isLive = false;
+						//change score
+						_this.onChangeScore();
+						return true;
+					}
+				}
+				return false;
+			}
+			//bullet end
+			bullet.onend = function(){
+				//remove from panel
+				_this.gamePanel.removeChild(this.dom);
+				//current bullets --
+				_this.nowBullet--;
+			}
+			//bullet move
+			bullet.animation();
+			//vullet++
+			_this.nowBullet++;
+		}
+	},
+	//player down
+	burstFlyer : function(){
+		this.dom.className = 'bingo';
+	},
+	//fire(outer interface, to get the enemy list from the game class)
+	onSendBullet : function(){},
+	//change score
+	onChangeScore : function(){}
+}
---
+++ Pure_JS/js/game.js
@@ -0,0 +1,180 @@
+//extend array function, delete, delete specific value
+Array.prototype.remove = function(obj){
+	
+	for(var i=0,l=this.length;i < l;i++){
+		if(this[i] == obj){
+			this.splice(i,1);
+			return this;
+		}
+	}
+	throw "The Array has no this Obj";
+}
+
+//Game control
+var Game = {
+	//Back ground dom
+	gamePanel : null,
+	//player
+	flyer : null,
+	//enemy list
+	enemyList : [],
+	//score
+	score : 0,
+	//is game over?
+	isGameOver : false,
+	//init
+	init : function(){
+		
+		var _this = this;
+		//get background
+		this.gamePanel = document.getElementById("gamePanel");
+		//focus on background
+		this.gamePanel.focus();
+		//gogogo!
+		this.startFlyer();
+		//enemy contact!
+		this.startEnemy();
+		//key press
+		document.body.onkeydown  = function(e){_this.onkeydown(e);};
+		document.body.onkeyup = function(e){_this.onkeyup(e);}
+	},
+	//start player
+	startFlyer : function(){
+		
+		var _this = this;
+		//new player object
+		this.flyer = new Flyer();
+		//set position
+		this.flyer.setPosition(this.gamePanel,this.gamePanel.clientWidth,this.gamePanel.clientHeight);
+		//fire function
+		this.flyer.onSendBullet = function(){this.sendBullet(_this.enemyList);};
+		//change score
+		this.flyer.onChangeScore = function(){_this.changeScore();};
+	},
+	//start enemy
+	startEnemy : function(){
+		//game over already
+		if(this.isGameOver)return;
+		
+		var _this = this;
+		//new enemy
+		var enemy = new Enemy();
+		//put into background
+		this.gamePanel.appendChild(enemy.dom);
+		//get a random x position for enemy show up
+		var randomX = parseInt(Math.random()* (this.gamePanel.clientWidth / enemy.dom.clientWidth),10);
+		//set the position
+		enemy.setPosition(randomX * enemy.dom.clientWidth,0);
+		//check hit
+		enemy.OnCheckCrash = function(){
+			//game over already
+			if(_this.isGameOver)return;
+			//hit or not
+			if(Math.sqrt(Math.pow(_this.flyer.dom.offsetLeft-this.dom.offsetLeft,2)+Math.pow(_this.flyer.dom.offsetTop-this.dom.offsetTop,2))
+				<= _this.flyer.dom.clientWidth/2 + this.dom.clientWidth/2){
+				//enemy down
+				this.isLive = false;
+				//if crash, life - 1
+				if (!_this.flyer.Lives) {
+					_this.flyer.burstFlyer();
+					return true;
+				}else{
+					_this.flyer.Lives = _this.flyer.Lives - 1;
+					document.getElementById('lives').innerHTML =  _this.flyer.Lives;
+					return false;
+				};
+				
+			}
+			return false;
+		}
+		//enemy remove
+		enemy.onend = function(){
+			_this.gamePanel.removeChild(this.dom);
+			_this.enemyList.remove(this);
+		}
+		//Mayday!Mayday!I'm shot!
+		enemy.manDown = function(){_this.manDown();}
+		//game over function
+		enemy.gameover = function(){_this.gameover();}
+		//enemy move
+		enemy.animation(this.gamePanel.clientWidth,this.gamePanel.clientHeight);
+		//add to enemy list
+		this.enemyList.push(enemy);
+		//start next enemy
+		setTimeout(function(){_this.startEnemy();},500);
+	},
+	//key press event
+	onkeydown : function(e){
+		e = e || window.event;
+		
+		var keyCode = e.keyCode;
+		//prevent default explorer
+		if(keyCode == 32 || this.flyer.keyCodeAndDirection[keyCode]){
+			if(e.preventDefault)e.preventDefault();
+			else e.returnValue = false;
+		}
+		else return;
+		//call key down function
+		this.flyer.keydown(e);
+	},
+	//key release
+	onkeyup : function(e){
+		e = e || window.event;
+		//call key release function
+		this.flyer.keyup(e);
+	},
+	//chenge score
+	changeScore : function(){
+		
+		this.score += 100;
+		document.getElementById('score').innerHTML =  this.score;
+		//score level
+		var scoreLevel = parseInt(this.score / 500,10) + 1;
+		//upgade score level and bonus life
+		if(scoreLevel > 1){
+			this.flyer.bulletLevel = scoreLevel>4?4:scoreLevel;
+			//change enemy speed
+			Enemy.prototype.movesp = Enemy.prototype.movespMap[this.flyer.bulletLevel];
+			Enemy.prototype.movepy = Enemy.prototype.movepyMap[this.flyer.bulletLevel];
+		}
+	},
+	manDown : function(){
+		if (this.flyer.Lives > 0) {
+			this.flyer.Lives = this.flyer.Lives - 1;
+			document.getElementById('lives').innerHTML =  this.flyer.Lives;
+		} else{
+			this.gameover();
+		};
+	},
+	//game over
+	gameover : function(){
+		
+		this.isGameOver = true;
+		
+		document.getElementById('score').innerHTML = "Game Over...You Score is:" + this.score;
+		
+		for(var i=0,l=this.enemyList.length;i < l;i++){
+			this.gamePanel.removeChild(this.enemyList[0].dom);
+			this.enemyList.remove(this.enemyList[0]);
+		}
+		
+		this.gamePanel.removeChild(this.flyer.dom);
+		this.flyer = null;
+		this.score = 0;
+		
+		this.gamePanel = null;
+		
+		document.body.onkeydown = null;
+		document.body.onkeyup = null;
+		
+		document.getElementById('startBtn').style.display = 'block';
+	}
+}
+//game start
+function Start(){
+	Game.isGameOver = false;
+	Game.init();
+	document.getElementById('startBtn').style.display = 'none';
+	document.getElementById('score').innerHTML = 0;
+	document.getElementById('lives').innerHTML = 3;
+}
